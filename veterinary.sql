db37d06909353fb190800777332d8c9e183a8c82
= <=>
2023-11-09 16:31:04 +0800

    feat/create-table-animals

--- veterinary.sql
+++ veterinary.sql
@@ -7,4 +7,14 @@ CREATE TABLE owner(
     address VARCHAR(100) NOT NULL,
     phone VARCHAR(15) NOT NULL,
     email VARCHAR(100) NOT NULL
-    );git
+    );
+
+    CREATE TABLE animals(
+   animalid INT PRIMARY KEY,
+   name VARCHAR(50) NOT NULL,
+   species VARCHAR(50) NOT NULL,
+   breed VARCHAR(50) NOT NULL,
+   dateofbirth DATE,
+   gender VARCHAR(10) NOT NULL,
+   color VARCHAR(50) NOT NULL,
+   ownerid INT,foreign key (ownerid) references owners(ownerid));
